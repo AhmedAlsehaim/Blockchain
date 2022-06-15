169122fbbd142f0bb3e9299199a8c7fd1f02e740
ahmed <absehaim@gmail.com>
2022-06-15 20:35:28 +0300

    feat: updated Block v2.0

--- Block.js
+++ Block.js
@@ -1,8 +1,11 @@
-class Block{
-    constructor(d, h, pH,tS){
+export default class Block{
+    constructor(data, hash, previousHash, timeStamp, height){
         this.data = d;
         this.hash = h;
         this.previousHash =pH;
         this.timeStamp = tS;
+        this.height = height;
+
     }
 }
+
